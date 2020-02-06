0af8aa8e962e6023f9941b1dd0cc803e36fc8810
Jonathan <jonathanrautenbach@gmail.com>
2020-02-06 08:09:30 +0200

    Layout setup

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,25 +1,9 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.app')
 
-        <title>Laravel 5.5</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <h1>{{$id}} {{$name}} {{$password}}</h1>
 
-        <!-- Styles -->
-        
-    </head>
-    <body>
-        <div class="container">
+@endsection
 
-            <H1>Post {{$id}} {{$name}} {{$password}}</H1>
-
-               
-            </div>
-        </div>
-    </body>
-</html>
+{{-- @stop --}}
